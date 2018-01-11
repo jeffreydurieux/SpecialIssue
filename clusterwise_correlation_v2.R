3f6836945606dc15d396e80a2145b673e2edd094
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2018-01-11 13:47:45 +0100

    add R-project

---
+++ clusterwise_correlation_v2.R
@@ -0,0 +1,15 @@
+#clusterwise correlation v2
+# Thu Jan 11 13:44:20 2018 ------------------------------
+
+# Tom's advice about how to generate correlated signal matrices
+
+library(ica)
+library(CICA)
+
+S <- replicate()
+
+clusterwise_correlation_v2 <- function(m1,m2,weight){
+  
+  
+  return(res)
+}
