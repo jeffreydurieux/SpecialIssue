bd4977248f34b1aad1e426c05f3e6b1851f841c1
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2018-01-12 16:50:59 +0100

    need finish simulationscript. (evaluation part of P, S and A)

--- SimulationScript.R
+++ SimulationScript.R
@@ -1,27 +1,56 @@
 # simulation script for special issue
 # Thu Jan 11 14:13:29 2018 ------------------------------
 
-
-<<<<<<< HEAD
-=======
+library(ica)
+library(CICA)
+library(mclust)
 source(file = "SimulateData.R")
 source(file = "clusterwise_correlation_v1.R")
 
 
->>>>>>> development
 
 ############ notes ################
 
 #between design
 
-clusters <- c(2,4)
-clustersize <- c("equal", "unequal")
-<<<<<<< HEAD
-error <- c(0.05, 0.30, 0.50)
-=======
+n_clusters <- c(2,4)
+c_size <- c("equal", "unequal")
 gauss <- c(1,0)
 error <- c(0.05, 0.30, 0.50)
 
-design <- expand.grid(clusters=clusters, clustersize = clustersize,
+design <- expand.grid(n_clusters=n_clusters, c_size = c_size,
                       gauss = gauss, error = error)
->>>>>>> development
+
+
+for(i in 1:24){
+  # generate data
+  data <- do.call(Sim_X, args = as.list(design[i,]) )
+  
+  
+  # analyze data
+  tmp <- proc.time()
+  cica <- CICA(nStarts = 30, DataList = data$Xe, nComp = 10, 
+               nClus = design$n_clusters[i], show = "best")
+  time <- proc.time() - tmp
+  
+  
+  # Evaluate simulation
+  ARI <- adjustedRandIndex(data$P, cica$P)
+  
+  #### congru of S
+  
+  #### congru of A
+  
+  
+  # save stuff on export
+  setwd("/exports/fsw/durieuxj/")
+  
+  # Write output, also possible to first save everything in a list object
+  save(data, file =paste("Simdata","Row", i, "Rep",Replication ,".Rdata" , sep ="")
+  save(Analysis, file =paste("Analysis","Row", i, "Rep",Replication ,".Rdata" , sep ="")
+  save(ARI, file =paste("Evaluation","Row", i, "Rep",Replication ,".Rdata" , sep ="")
+  save(time, file =paste("Time","Row",i, "Rep",Replication ,".Rdata" , sep ="")
+  
+}
+
+
