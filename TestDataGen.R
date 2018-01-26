741957b7e069a776f84310956e22e248cf9fd699
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2018-01-26 14:46:46 +0100

    remove

--- TestDataGen.R
+++ TestDataGen.R
@@ -4,69 +4,108 @@ library(MatrixCorrelation)
 library(mclust)
 library(apcluster)
 library(cluster)
-library(distr)
 
-# D <- DExp(rate = 1)
-# D1 <- D+1
-# D2 <- D+2
-# D5 <- D+5
-# 
-# 
-# Sbase <- replicate(n = 10, expr = r(D)(1000))
-# 
-# SQ1 <- replicate(n = 10, r(D5)(1000))
-# SQ2 <- replicate(n = 10, r(D5)(1000))
-# 
-# S1 <- Sbase + SQ1
-# S2 <- Sbase + SQ2
 
-# RV2(S1,S2)
-# apply(abs(congru(S1,S2)), 2,max)
+# con explanation: con .15 is a runif(n, -.15, .15) results in a high correlation between S
+# con 1 results in a low correlation
 
-conH <- .15
-conM <- .3
-conL <- 2
+n_clusters <- c(2,4)
+c_size <- c("equal", "unequal")
+con <- c(.15, .4, 1)
+error <- c(0.1, 0.50, 0.90)
 
-Sbase <- replicate(n = 10, expr = runif(n = 1000, min = -1, max = 1))
+design <- expand.grid(n_clusters = n_clusters, c_size = c_size,
+                      con = con, error = error)
 
-SQ1 <- replicate(n = 10, runif(n = 1000, min = -conH, max = conH))
-SQ2 <- replicate(n = 10, runif(n = 1000, min = -conH, max = conH))
- 
-S1 <- Sbase + SQ1
-S2 <- Sbase + SQ2
- 
-RV2(S1,S2)
-#apply(abs(congru(S1,S2)), 2,max)
+Sim_A <- function(nscan, n_signals){
+  TS <- matrix(data = NA, nrow = nscan, ncol = n_signals)
+  for(nts in 1:n_signals){# add a piece of code such that NaN's do not occur
+    options(warn = -1) #temp warning supression
+    repeat{
+      TS[ ,nts] <- neuRosim::simTSrestingstate(nscan = nscan, TR = 2, noise = "none")
+      if(any(is.finite(TS[,nts])) == TRUE){
+        break
+      }#end break
+    }#end repeat
+  }# end for nts
+  options(warn = 0) #turn warnings on
+  return(TS)
+}
 
-A1 <- replicate(10, Sim_A(nscan = 100,n_signals = 10), simplify = F)
-A2 <- replicate(10, Sim_A(nscan = 100,n_signals = 10), simplify = F)
+SimData <- function(n_clusters, c_size, con, error){
+  
+  Sbase <- replicate(n = 20, expr = runif(n = 1000, min = -1, max = 1))
+  
+  Alist <- list()
+  Slist <- list()
+  Xlist <- list()
+  for(i in 1:n_clusters){
+    if(c_size == 'equal'){
+      Alist[[i]] <- replicate( (60 / n_clusters) , 
+                               Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      
+      Slist[[i]] <- replicate(n = 20, 
+                              runif(n = 1000, min = -con, max = con))
+      
+    }else if(c_size == 'unequal' & n_clusters == 2){
+      Alist[[1]] <- replicate( 15 , Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      Alist[[2]] <- replicate( 45 , Sim_A(nscan = 100,n_signals = 20), simplify = F)
+      
+      Slist[[i]] <- replicate(n = 20, 
+                              runif(n = 1000, min = -con, max = con))
+    }else if(c_size == 'unequal' & n_clusters == 4){
+      Alist[[1]] <- replicate( 5 , Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      Alist[[2]] <- replicate( 10 , Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      Alist[[3]] <- replicate( 20 , Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      Alist[[4]] <- replicate( 25 , Sim_A(nscan = 100,n_signals = 20), simplify = F) 
+      
+      Slist[[i]] <- replicate(n = 20, 
+                              runif(n = 1000, min = -con, max = con))
+    }
+    
+    S <- lapply(seq_along(Slist), function(x) Sbase + Slist[[x]])
+    
+    Xlist[[i]] <- lapply(seq_along(Alist[[i]]) , function(x) S[[i]] %*% t(Alist[[i]][[x]]))
+    
+  }
+  
+  X <- unlist(Xlist, recursive = F)
+  XE <- lapply(X, FUN = addError, error = error)
+  
+  data <- list()
+  data$Xe <- XE
+  data$X <- X
+  data$S <- S
+  data$A <- Alist
+  pid <- sapply(Alist, length)
+  if(n_clusters == 2){
+    data$P <- c(rep(1,pid[1]), rep(2,pid[2]))  
+  }else{
+    data$P <- c(rep(1,pid[1]), rep(2,pid[2]), rep(3,pid[3]), rep(4,pid[4]))
+  }
+  
+  return(data)
+}
 
-X1 <- lapply(seq_along(A1), function(x) S1%*%t(A1[[x]]))
-X2 <- lapply(seq_along(A2), function(x) S2%*%t(A2[[x]]))
+test <- do.call(SimData, args = as.list(design[4,]))
+test$P
 
-X <- c(X1,X2)
-Xe <- lapply(X, FUN = addError, error=0.1)
+icaL <- lapply(data$Xe, FUN = icafast, nc = 20)
 
-data <- list()
-data$Xe <- Xe
-data$P <- c(rep(1,10), rep(2,10))
-
-icaL <- lapply(data$Xe, FUN = icafast, nc = 10)
-
-comb <- t(combn(1:20, 2))
+comb <- t(combn(1:60, 2))
 M <- lapply(icaL, function(anom) anom$M)
 S <- lapply(icaL, function(anom) anom$S)
 
 time_RVM <- proc.time()
 RVM <- numeric()
-for(i in 1:190){
+for(i in 1:1770){
   RVM[i] <- RV2(M[[ comb[i,1] ]] , M[[ comb[i,2] ]])
 }
 time_RVM <- proc.time() - time_RVM
 
 time_RVS <- proc.time()
 RVS <- numeric()
-for(i in 1:190){
+for(i in 1:1770){
   RVS[i] <- RV2(S[[ comb[i,1] ]] , S[[ comb[i,2] ]])
   #cat(i,"\n")
 }
@@ -75,10 +114,10 @@ time_RVS <- proc.time() - time_RVS
 resM <- cbind(comb,RVM)
 resS <- cbind(comb,RVS)
 
-RVsM <- matrix(data = NA, nrow = 20 , ncol = 20)
-RVsS <- matrix(data = NA, nrow = 20 , ncol = 20)
+RVsM <- matrix(data = NA, nrow = 60 , ncol = 60)
+RVsS <- matrix(data = NA, nrow = 60 , ncol = 60)
 
-for(i in 1:190){
+for(i in 1:1770){
   val <- resM[i,]
   RVsM[val[1]  , val[2] ] <- val[3]
   
@@ -104,17 +143,17 @@ time_apq0.5 <- proc.time()
 apq0.5 <- apcluster(s = SIMMAT)
 time_apq0.5 <- proc.time() - time_apq0.5
 
-apq0.5lab <- 1:10
+apq0.5lab <- 1:60
 for(i in 1:length(apq0.5@clusters)){
   apq0.5lab <- replace(apq0.5lab, apq0.5@clusters[[i]], i)  
 }
 
 
 time_apq0.1 <- proc.time()
-apq0.1 <- apcluster(s = SIMMAT, q = 0.1)
+apq0.1 <- apcluster(s = SIMMAT, q = 0.5)
 time_apq0.1 <- proc.time() - time_apq0.1
 
-apq0.1lab <- 1:10
+apq0.1lab <- 1:60
 for(i in 1:length(apq0.1@clusters)){
   apq0.1lab <- replace(apq0.1lab, apq0.1@clusters[[i]], i)  
 }
